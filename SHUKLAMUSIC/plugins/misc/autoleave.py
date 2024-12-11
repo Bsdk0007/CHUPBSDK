abcc8c4ba1a7eb5019f16f523b6f284981921602
Bsdk0007 <princek8356@gmail.com>
2024-12-11 07:32:49 +0530

    Update autoleave.py

--- SHUKLAMUSIC/plugins/misc/autoleave.py
+++ SHUKLAMUSIC/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1002141133985
-                                and i.chat.id != -1002066887457
+                                and i.chat.id != -1002335312098
+                                and i.chat.id != -1001992970818
                             ):
                                 if left == 20:
                                     continue
